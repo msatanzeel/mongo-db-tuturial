3765f7521421dedcff553c12b7816c6b40fa9551
MohammadSajidAliTanzeel <mdtanzeel.sajid@sprinklr.com>
2024-02-24 22:18:02 +0530

    setup: Initial commit

---
+++ src/main/java/com/tanzeel/mongodbtutorial/MongoDbTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.tanzeel.mongodbtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDbTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tanzeel/mongodbtutorial/MongoDbTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanzeel.mongodbtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
